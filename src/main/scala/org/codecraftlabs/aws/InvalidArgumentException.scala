e3f1ee84fb2e922ac3b455e464818b8e7dea0696
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-02-19 20:33:27 +0100

    remove unused class

--- src/main/scala/org/codecraftlabs/aws/InvalidArgumentException.scala
+++
@@ -1,4 +0,0 @@
-package org.codecraftlabs.aws
-
-case class InvalidArgumentException(private val message: String = "",
-                                    private val cause: Throwable = None.orNull) extends Exception (message, cause)
